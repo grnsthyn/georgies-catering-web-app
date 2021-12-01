2e602b27d03fbbf66eb26e3385a2110392f6c57d
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 20:21:20 +0800

    setup mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require('./app')
+const connectDatabase = require('./config/database')
 
 
 const dotenv = require('dotenv');
@@ -6,6 +7,8 @@ const dotenv = require('dotenv');
 // Setting up config file path
 dotenv.config({ path: 'backend/config/config.env' })
 
+connectDatabase();
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode. `)
+    console.log(`Server started on PORT: ${process.env.POR } in ${process.env.NODE_ENV} mode. `)
 })
