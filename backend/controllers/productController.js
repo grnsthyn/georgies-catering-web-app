b8b030a96b12fe8d01113f294d08caba27b8f7bf
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 22:22:13 +0800

    search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,13 @@
 const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const APIFeatures = require('../utils/apiFeatures')
 
 
 // create new product => api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
-    const product =  await Product.create(req.body)
+
+        const product =  await Product.create(req.body)
 
     res.status(201).json({
         success: true,
@@ -13,10 +15,13 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
     })
 })
 
-// Get all products => /api/v1/products
+// Get all products => /api/v1/products?keyword=menudo
 exports.getProducts = catchAsyncErrors(async  (req, res, next) => {
 
-    const products = await Product.find();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+                                            .search()
+
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
