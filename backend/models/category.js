dfcd8323b773942199ef986d97dedc8fa92a46ec
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 20:35:54 +0800

    product/category schema

---
+++ backend/models/category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema({
+   
+    category: {
+        type: String, 
+        required: [true, 'Please enter product category'],
+        trim: true
+    }
+    
+   
+    
+})
+
+
+module.exports = mongoose.model('Category', categorySchema);
---
+++ backend/models/product.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxLength:[100, 'Please enter name maximum of 100 hehehe']
+    },
+    price: {
+        type: String, 
+        required: [true, 'Please enter product price'],
+        trim: true,
+        maxLength:[7, 'Please enter name maximum of 100 hehehe'],
+        default: 0.0
+    },
+    size: {
+        type: String, 
+        required: [true, 'Please enter product size'],
+        trim: true,
+        maxLength:[10, 'Please enter name maximum of 100 hehehe']
+    },
+    available: {
+        type: Boolean, 
+        required: [true, 'Please enter product availability'],
+        trim: true,
+    },
+    image: [
+        { 
+            public_id:{
+                type: String, 
+                required: true
+            },
+            url:{
+                type: String, 
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String, 
+        required: [true, 'Please enter product category'],
+        trim: true
+    }
+    
+   
+    
+})
+
+
+module.exports = mongoose.model('Product', productSchema);
