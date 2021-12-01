eadd62819f05e77bf73f22e67bb8083a3cf2597f
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 22:32:37 +0800

    filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,6 +20,7 @@ exports.getProducts = catchAsyncErrors(async  (req, res, next) => {
 
     const apiFeatures = new APIFeatures(Product.find(), req.query)
                                             .search()
+                                                .filter()
 
     const products = await apiFeatures.query;
 
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,12 +1,12 @@
 class APIFeatures {
-    constructor(query, queryStr){
+    constructor(query, queryStr) {
         this.query = query;
         this.queryStr = queryStr;
     }
 
-    search(){
+    search() {
         const keyword = this.queryStr.keyword ? {
-            name:{
+            name: {
                 $regex: this.queryStr.keyword,
                 $options: 'i'
             }
@@ -15,6 +15,22 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filter() {
+        const queryCopy = { ...this.queryStr };
+
+        // Removing fields from the query 
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        // Advance filter for price, size, etc
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+        this.query = this.query.find(JSON.parse(queryCopy));
+        return this;
+    }
+
 }
 
 module.exports = APIFeatures
