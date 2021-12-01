339be227c22ede6057e74e85e314853b45d731d4
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 23:53:17 +0800

    jwt token assign

--- backend/models/user.js
+++ backend/models/user.js
@@ -44,8 +44,14 @@ userSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, 10)
 })
 
+// compare user password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+
 // return JWT Token
-userSchema.methods.getJwtToken = function () {
+userSchema.methods.getJwtToken = function() {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_TIME
     });
