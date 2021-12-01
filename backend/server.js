fb0cedbd16c056c4affe26b1fb4190fdc45683bd
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 21:55:31 +0800

    Unhandled Promise rejections

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,15 @@ dotenv.config({ path: 'backend/config/config.env' })
 
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode. `)
+})
+
+// Handle Unhandled Promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log('Shutting down the server due to Unhandled Promise rejections');
+    server.close(() =>{
+        process.exit(1)
+    })
 })
