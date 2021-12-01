f9b0fd4c376cc7fd5cbfac4f169d20dcee15668e
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 21:51:30 +0800

    catchAsyncError

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,19 +1,20 @@
 const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 
 
 // create new product => api/v1/admin/product/new
-exports.newProduct = async(req, res, next) => {
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
     const product =  await Product.create(req.body)
 
     res.status(201).json({
         success: true,
         product
     })
-}
+})
 
 // Get all products => /api/v1/products
-exports.getProducts = async  (req, res, next) => {
+exports.getProducts = catchAsyncErrors(async  (req, res, next) => {
 
     const products = await Product.find();
 
@@ -22,11 +23,11 @@ exports.getProducts = async  (req, res, next) => {
         count: products.length,
         products
     })
-}
+})
 
 // Get single products => /api/v1/product/:id
 
-exports.getSingleProduct = async (req, res, next) => {
+exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
     if(!product){
@@ -37,11 +38,11 @@ exports.getSingleProduct = async (req, res, next) => {
         success:true,
         product
     })
-}
+})
 
 // Update product => /api/v1/admin/product/:id
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
 
     let product = await Product.findById(req.params.id);
     
@@ -59,10 +60,10 @@ exports.updateProduct = async (req, res, next) => {
         success: true,
         product
     })   
-}
+})
 
 // delete product => /api/v1/admin/product/:id
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
 
     const product = await Product.findById(req.params.id);
 
@@ -76,4 +77,4 @@ exports.deleteProduct = async (req, res, next) => {
         success: true,
         message: 'Product is deleted successfully'
     })
-}
+})
