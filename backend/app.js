7551831b554cbb72b7b0896e8f999cd01522fe73
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-02 00:10:56 +0800

    protected routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+
+
 const errorMiddleware = require('./middlewares/errors')
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 
