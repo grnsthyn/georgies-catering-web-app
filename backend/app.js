2ad1a3b6ebb8ba993c90cd2505cda42f32c69e7c
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 23:17:44 +0800

    update to user

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,11 @@ app.use(express.json());
 
 // Import all routes from
 const products = require('./routes/product');
+const auth = require('./routes/auth');
 
 
 app.use('/api/v1', products)
+app.use('/api/v1', auth)
 
 
 // Middleware to handle error
---
+++ backend/controllers/authController.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+
+const ErrorHandler = require('../utils/errorHandler')
+
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+
+// Register a user => /api/v1/registered users
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+
+    const { full_name, email, password } = req.body;
+
+    const user = await User.create(req.body)
+
+    res.status(201).json({
+        success: true,
+        user
+    })
+
+})
+
+// Encrypting password before saving user
