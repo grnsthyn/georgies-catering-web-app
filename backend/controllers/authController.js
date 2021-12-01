f10781cba646608cf7c890ed54e47a8cae4483ca
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-02 01:01:28 +0800

    reset password recovery

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler')
+const sendEmail = require('../utils/sendEmail')
 
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
@@ -42,6 +43,46 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 200, res)
 
+})
+
+// forgot password => /a
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if(!user){
+        return next(new ErrorHandler('User not found with this email', 404));
+    }
+
+    // get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false })
+
+    // create reset password urlencoded
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
+    const message = `Your password reset token is as follows:\n\n${resetUrl}\n\nIf You have not requested this email, then ignore this message.`
+
+    try{
+
+        await sendEmail({
+            email: user.email, 
+            subject: 'Georgies Catering Password Recovery',
+            message
+        })
+
+        res.status(200).json({ 
+            success: true,
+            message: `Email sent to ${user.email}`
+        })
+
+    }catch(error){
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500))
+    }
 })
 
 // Logout user => api/v1/Logout
