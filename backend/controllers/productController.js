f6af61b7cb9b1e44061fdda8eb428cf1eb5fe6b4
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-01 22:40:30 +0800

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,9 +18,12 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 // Get all products => /api/v1/products?keyword=menudo
 exports.getProducts = catchAsyncErrors(async  (req, res, next) => {
 
+    const resPerPage = 3;
+
     const apiFeatures = new APIFeatures(Product.find(), req.query)
                                             .search()
                                                 .filter()
+                                                    .pagination(resPerPage)
 
     const products = await apiFeatures.query;
 
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -31,6 +31,14 @@ class APIFeatures {
         return this;
     }
 
+    pagination(resPerPage){
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resPerPage).skip(skip);
+        return this;
+    }
+
 }
 
 module.exports = APIFeatures
