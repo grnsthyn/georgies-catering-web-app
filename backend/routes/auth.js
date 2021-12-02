1b886079e76f2fd677ff50168aa3cf97042184ac
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-12-02 12:22:06 +0800

    admin, get user detail

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,9 +7,11 @@ const { registerUser,
         resetPassword,
         updatePassword,
         getUserProfile,
-        updateProfile} = require('../controllers/authController');
+        updateProfile,
+        allUsers,
+        getUserDetails} = require('../controllers/authController');
 
-const { isAuthenticatedUser } = require('../middlewares/auth') 
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth') 
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
@@ -20,6 +22,10 @@ router.route('/password/reset/:token').put(resetPassword)
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+
 router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails);
 
 module.exports = router; 
